972eb8d1882d51c23509b9ff4a053e4a301080a6
Anna Akimova <aak@sitecore.net>
2021-04-09 11:28:54 +0300

    Init commit

---
+++ src/Sitecore.Support.210355/Analytics/RobotDetection/layouts/System/VisitorIdentificationCss.cs
@@ -0,0 +1,45 @@
+﻿using Sitecore.Analytics;
+using Sitecore.Analytics.Configuration;
+using Sitecore.Analytics.Core;
+using Sitecore.Analytics.RobotDetection;
+using Sitecore.Analytics.Web;
+using Sitecore.Diagnostics;
+using Sitecore.Xdb.Configuration;
+using System;
+using System.Web.UI;
+
+namespace Sitecore.Analytics.RobotDetection.layouts.system
+{
+    public class VisitorIdentificationCss : System.Web.UI.Page
+    {
+        protected override void OnInit(EventArgs e)
+        {
+            Assert.ArgumentNotNull(e, "e");
+            base.OnInit(e);
+            base.Response.ContentType = "text/css";
+            if (XdbSettings.Tracking.Enabled && Tracker.Current.Sampling.IsSampling() && Tracker.IsActive)
+            {
+                Tracker.Current.CurrentPage.Cancel();
+                if (Tracker.Current.Interaction.PageCount != 1 && AnalyticsSettings.Robots.AutoDetect)
+                {
+                    UpdateVisitorIdentification();
+                    SessionUtil.ResetSessionTimeout();
+                }
+            }
+        }
+
+        private void UpdateVisitorIdentification()
+        {
+            ContactKeyCookie contactKeyCookie = new ContactKeyCookie();
+            if (!contactKeyCookie.IsClassificationGuessed)
+            {
+                contactKeyCookie.IsClassificationGuessed = true;
+                contactKeyCookie.Update();
+                if (Tracker.Current.Session != null && ContactClassification.IsAutoDetectedRobot(Tracker.Current.Session.Contact.System.Classification))
+                {
+                    Tracker.Current.Session.SetClassification(0, 0, isContactClassificationGuessed: true);
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.210355/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.210355")]
+[assembly: AssemblyProduct("Sitecore.Support.210355")]
+[assembly: ComVisible(false)]
